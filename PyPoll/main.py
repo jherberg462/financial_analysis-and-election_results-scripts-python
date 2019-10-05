aee0555e363bed53886556450e87a0a4a10037ca
Jeremiah Herberg <jherberg88@me.com>
2019-10-05 19:08:42 -0400

    Add existing file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,6 +24,7 @@ canidate = []
 datapath = "data/election_data.csv"
 with open (datapath, newline='') as csv_results: #open csv file via path defined in above line
     csv_read = csv.reader(csv_results, delimiter=',')
+    csvheader = next(csv_read)
     for row in csv_read: 
         voter_id.append(row[0]) #create lists with voter ID, county and the canidate voted for
         county.append(row[1]) #look at data to see if we need to import all this data
